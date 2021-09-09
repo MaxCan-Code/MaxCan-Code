MaxCantCode/MaxCantCode.github.io

\left as an exercise

symlinks/foo/.config/julia/config/startup.jl
@@ -1,0 +1,1 @@
+ENV["JULIA_EDITOR"] = "vim"

MaxCan-Code/MaxCan-Code

burning-down-za-house/.config/julia/config/startup.jl
@@ -1,0 +1,1 @@
+ENV["JULIA_EDITOR"] = "emacs"